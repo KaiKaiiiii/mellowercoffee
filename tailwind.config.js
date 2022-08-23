74b4e05c8cfa14b1ad7fd09018c8a61479cf611a
KaiKaiiiii <90381850+KaiKaiiiii@users.noreply.github.com>
2022-08-23 18:21:34 +0700

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,12 @@ module.exports = {
       borderWidth: {
         2: "1px",
       },
+      margin: {
+        100: "100px",
+      },
+      width: {
+        52: "11rem",
+      },
       colors: {
         gray: "#958B8B",
         gray88: "#888888",
