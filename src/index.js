2e7ef8768776c51da3d9581d52b59fbf56cd9903
KaiKaiiiii <90381850+KaiKaiiiii@users.noreply.github.com>
2022-08-28 16:04:19 +0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -21,41 +21,44 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App></App>}>
-        <Route path="/" element={<HomePage></HomePage>}></Route>
-        <Route path="/beans" element={<Beans></Beans>}></Route>
-        <Route path="beans/africa" element={<Africa></Africa>}></Route>
-        <Route path="beans/america" element={<America></America>}></Route>
-        <Route path="beans/asia" element={<Asia></Asia>}></Route>
-        <Route
-          path="beans/geisha"
-          element={<GeishaDetail></GeishaDetail>}
-        ></Route>
-        <Route
-          path="beans/aromatic"
-          element={<AromaticDetail></AromaticDetail>}
-        ></Route>
-        <Route
-          path="beans/eyunnan"
-          element={<EYunnanDetail></EYunnanDetail>}
-        ></Route>
-        <Route
-          path="beans/colombia"
-          element={<ColombiaDetail></ColombiaDetail>}
-        ></Route>
-        <Route
-          path="beans/sumatra"
-          element={<SumatraDetail></SumatraDetail>}
-        ></Route>
-        <Route
-          path="beans/ethiopia"
-          element={<EthiopiaDetail></EthiopiaDetail>}
-        ></Route>
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <Suspense fallback={<div>Loading..</div>}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App></App>}>
+          <Route path="/" element={<HomePage></HomePage>}></Route>
+
+          <Route path="/beans" element={<Beans></Beans>}></Route>
+          <Route path="beans/africa" element={<Africa></Africa>}></Route>
+          <Route path="beans/america" element={<America></America>}></Route>
+          <Route path="beans/asia" element={<Asia></Asia>}></Route>
+          <Route
+            path="beans/geisha"
+            element={<GeishaDetail></GeishaDetail>}
+          ></Route>
+          <Route
+            path="beans/aromatic"
+            element={<AromaticDetail></AromaticDetail>}
+          ></Route>
+          <Route
+            path="beans/eyunnan"
+            element={<EYunnanDetail></EYunnanDetail>}
+          ></Route>
+          <Route
+            path="beans/colombia"
+            element={<ColombiaDetail></ColombiaDetail>}
+          ></Route>
+          <Route
+            path="beans/sumatra"
+            element={<SumatraDetail></SumatraDetail>}
+          ></Route>
+          <Route
+            path="beans/ethiopia"
+            element={<EthiopiaDetail></EthiopiaDetail>}
+          ></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </Suspense>
 );
 
 // If you want to start measuring performance in your app, pass a function
